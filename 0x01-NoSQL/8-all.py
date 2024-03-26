e98d1a9e43e6b7d5789d5a15cadfb2627824d286
RichardMiruka <richardonsare15@gmail.com>
2024-03-26 15:54:15 +0300

    a Python function that inserts a new document in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,15 +1,12 @@
- #!/usr/bin/env python3
-"""
-List all documents in Python
-"""
-import pymongo
+#!/usr/bin/env python3
+""" 8-main """
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
 
-
-def list_all(mongo_collection):
-    """
-    function to list all doc in a collection
-    """
-    if not mongo_collection:
-        return []
-    documents = mongo_collection.find()
-    return [post for post in documents]
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {}".format(school.get('_id'), school.get('name')))
+        
